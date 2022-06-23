c769a6fc6a82f6556ee059fd3773b82155c766c9
Kalai <ssokalai@gmail.com>
2022-06-23 19:53:52 +0800

    feat(backend): WIP login

--- cahoo-backend/src/main/java/com/kalaiz/cahoo_backend/SecurityConfiguration.java
+++ cahoo-backend/src/main/java/com/kalaiz/cahoo_backend/SecurityConfiguration.java
@@ -8,15 +8,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Override
     protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-			// TODO
+			auth
+		    .inMemoryAuthentication()
+	          .withUser("user")
+	          .password("password")
+	          .roles("USER");
     }
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-    	// TODO
+    	http.csrf().disable().authorizeRequests()
+    	.anyRequest().authenticated().and().formLogin();
+    	
     }
+    
 }
